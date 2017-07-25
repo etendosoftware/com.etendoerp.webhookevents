fc7cf6a71ebfb7f5bdde69863b21454741bf30c5
juabaez <juabaez@gmail.com>
2017-07-25 18:40:59 -0300

    API-4 se hicieron las peticiones a slack para realizar una prueba, tambien se creo la libreria que se encarga de eso.

--- src/smf/webhookevents/webhook_util/WebHookUtil.java
+++ src/smf/webhookevents/webhook_util/WebHookUtil.java
@@ -1,6 +1,8 @@
 package smf.webhookevents.webhook_util;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
@@ -34,6 +36,8 @@ public class WebHookUtil {
   public final static String START_PARAMETER = "?";
   public final static String AMPERSAND = "&";
   public final static String EQUALS = "=";
+  public final static String STRING_JSON = "JSON";
+  public final static String STRING_XML = "XML";
 
   /* Call the all webhook defined in this event */
   public static void callWebHook(Events event, BaseOBObject bob) throws Exception {
@@ -65,7 +69,12 @@ public class WebHookUtil {
     con.setRequestProperty("Content-Type", CONTENT_TYPE);
 
     // Verify if can data is json or xml
-    String postJsonData = generateDataParametersJSON(hook.getSmfwheStdparamList(), bob);
+    String postJsonData = "";
+    if (hook.getTypedata().equals(STRING_JSON)) {
+      postJsonData = generateDataParametersJSON(hook.getSmfwheStdparamList(), bob);
+    } else if (hook.getTypedata().equals(STRING_XML)) {
+      postJsonData = generateDataParametersXML(hook.getSmfwheStdparamList(), bob);
+    }
 
     // Send post request
     con.setDoOutput(true);
@@ -74,19 +83,23 @@ public class WebHookUtil {
     wr.flush();
     wr.close();
 
-    /*
-     * int responseCode = con.getResponseCode();
-     * System.out.println("nSending 'POST' request to URL : " + url);
-     * System.out.println("Post Data : " + postJsonData); System.out.println("Response Code : " +
-     * responseCode);
-     * 
-     * BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream())); String
-     * output; StringBuffer response = new StringBuffer();
-     * 
-     * while ((output = in.readLine()) != null) { response.append(output); } in.close();
-     * 
-     * // printing result from response System.out.println(response.toString());
-     */
+    int responseCode = con.getResponseCode();
+    System.out.println("nSending 'POST' request to URL : " + url);
+    System.out.println("Post Data : " + postJsonData);
+    System.out.println("Response Code : " + responseCode);
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+    String output;
+    StringBuffer response = new StringBuffer();
+
+    while ((output = in.readLine()) != null) {
+      response.append(output);
+    }
+    in.close();
+
+    // printing result from response
+    System.out.println(response.toString());
+
   }
 
   /*
