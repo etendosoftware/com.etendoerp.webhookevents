759b01e9b63b0b171e23f1c684861bcd2312555f
juabaez <juabaez@gmail.com>
2017-07-25 17:26:10 -0300

    API-4 se hicieron las peticiones a slack para realizar una prueba, tambien se creo la libreria que se encarga de eso.

--- src/smf/webhookevents/ad_event/BusinessPartnerHook.java
+++ src/smf/webhookevents/ad_event/BusinessPartnerHook.java
@@ -17,13 +17,17 @@ import org.openbravo.dal.service.OBDal;
 import org.openbravo.model.ad.datamodel.Table;
 import org.openbravo.model.common.businesspartner.BusinessPartner;
 
-import com.smf.webhookevents.data.Events;
-import com.smf.webhookevents.data.Webhook;
+import smf.webhookevents.webhook_util.WebHookUtil;
 
-public class BusinessPartnerSlack extends EntityPersistenceEventObserver {
+import com.smf.webhookevents.data.Events;
+
+public class BusinessPartnerHook extends EntityPersistenceEventObserver {
   private static Entity[] entities = { ModelProvider.getInstance().getEntity(
       BusinessPartner.ENTITY_NAME) };
   protected Logger logger = Logger.getLogger(this.getClass());
+  private final static String CREATE = "C";
+  private final static String UPDATE = "U";
+  private final static String DELETE = "D";
 
   @Override
   protected Entity[] getObservedEntities() {
@@ -36,16 +40,26 @@ public class BusinessPartnerSlack extends EntityPersistenceEventObserver {
     }
     logger.info("Business Partner " + ((BusinessPartner) event.getTargetInstance()).getName()
         + " is being created");
+    try {
+      final BusinessPartner bPartner = (BusinessPartner) event.getTargetInstance();
+      OBCriteria<Events> cEvents = OBDal.getInstance().createCriteria(Events.class);
+      cEvents.createAlias(Events.PROPERTY_TABLE, "table");
+      cEvents.add(Restrictions.eq(Events.PROPERTY_ACTIVE, true));
+      cEvents.add(Restrictions.eq(Events.PROPERTY_EXECUTEON, CREATE));
+      cEvents.add(Restrictions
+          .eq("table." + Table.PROPERTY_DBTABLENAME, BusinessPartner.TABLE_NAME));
 
-    final BusinessPartner bPartner = (BusinessPartner) event.getTargetInstance();
-    OBCriteria<Events> cEvents = OBDal.getInstance().createCriteria(Events.class);
-    cEvents.createAlias(Events.PROPERTY_TABLE, "table");
-    cEvents.add(Restrictions.eq(Events.PROPERTY_ACTIVE, true));
-    cEvents.add(Restrictions.eq(Events.PROPERTY_EXECUTEON, "C"));
-    cEvents.add(Restrictions.eq("table." + Table.PROPERTY_DBTABLENAME, BusinessPartner.TABLE_NAME));
-
-    List<Events> lEvents = cEvents.list();
-
+      List<Events> lEvents = cEvents.list();
+      if (!lEvents.isEmpty()) {
+        if (lEvents.get(0).isAllrecord()) {
+          WebHookUtil.callWebHook(lEvents.get(0), bPartner);
+        } else {
+          // ACA IRIA LA PARTE DEL CRITERIA EVENT
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public void onUpdate(@Observes EntityUpdateEvent event) {
@@ -60,15 +74,6 @@ public class BusinessPartnerSlack extends EntityPersistenceEventObserver {
       return;
     }
     logger.info("Business Partner " + event.getTargetInstance().getId() + " is being deleted");
-  }
-
-  private static void callWebHook(Events event) {
-    OBCriteria<Webhook> cWebhook = OBDal.getInstance().createCriteria(Webhook.class);
-    cWebhook.add(Restrictions.eq(Webhook.PROPERTY_SMFWHEEVENTS, event));
-
-    for (Webhook hook : cWebhook.list()) {
-
-    }
   }
 
 }
---
+++ src/smf/webhookevents/webhook_util/WebHookUtil.java
@@ -0,0 +1,137 @@
+package smf.webhookevents.webhook_util;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.hibernate.criterion.Restrictions;
+import org.openbravo.base.structure.BaseOBObject;
+import org.openbravo.dal.core.DalUtil;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBCriteria;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.database.ConnectionProvider;
+import org.openbravo.erpCommon.utility.Utility;
+import org.openbravo.service.db.DalConnectionProvider;
+
+import com.smf.webhookevents.data.Customparam;
+import com.smf.webhookevents.data.Events;
+import com.smf.webhookevents.data.StandardParameter;
+import com.smf.webhookevents.data.Webhook;
+
+public class WebHookUtil {
+  final private static String language = OBContext.getOBContext().getLanguage().getLanguage();
+  final private static ConnectionProvider conn = new DalConnectionProvider(false);
+
+  public final static String USER_AGENT = "Mozilla/5.0";
+  public final static String ACCEPT_LANGUAGE = "en-US,en;q=0.5";
+  public final static String CONTENT_TYPE = "application/json";
+  public final static String START_PARAMETER = "?";
+  public final static String AMPERSAND = "&";
+  public final static String EQUALS = "=";
+
+  /* Call the all webhook defined in this event */
+  public static void callWebHook(Events event, BaseOBObject bob) throws Exception {
+    OBCriteria<Webhook> cWebhook = OBDal.getInstance().createCriteria(Webhook.class);
+    cWebhook.add(Restrictions.eq(Webhook.PROPERTY_SMFWHEEVENTS, event));
+
+    for (Webhook hook : cWebhook.list()) {
+      try {
+        if (hook.isActive()) {
+          sendEvent(hook, bob);
+        }
+      } catch (Exception e) {
+        throw new Exception(e);
+      }
+    }
+  }
+
+  /* Send the request to url to notify defined in webhook */
+  public static void sendEvent(Webhook hook, BaseOBObject bob) throws Exception {
+
+    String url = hook.getUrlnotify() + generateCustomParameter(hook.getSmfwheCustomparamList());
+    URL obj = new URL(url);
+    HttpURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+    // Setting basic post request
+    con.setRequestMethod(hook.getSmfwheEvents().getMethod());
+    con.setRequestProperty("User-Agent", USER_AGENT);
+    con.setRequestProperty("Accept-Language", ACCEPT_LANGUAGE);
+    con.setRequestProperty("Content-Type", CONTENT_TYPE);
+
+    // Verify if can data is json or xml
+    String postJsonData = generateDataParametersJSON(hook.getSmfwheStdparamList(), bob);
+
+    // Send post request
+    con.setDoOutput(true);
+    DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+    wr.writeBytes(postJsonData);
+    wr.flush();
+    wr.close();
+
+    /*
+     * int responseCode = con.getResponseCode();
+     * System.out.println("nSending 'POST' request to URL : " + url);
+     * System.out.println("Post Data : " + postJsonData); System.out.println("Response Code : " +
+     * responseCode);
+     * 
+     * BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream())); String
+     * output; StringBuffer response = new StringBuffer();
+     * 
+     * while ((output = in.readLine()) != null) { response.append(output); } in.close();
+     * 
+     * // printing result from response System.out.println(response.toString());
+     */
+  }
+
+  /*
+   * Generate a JSON data parameter, take a StandardParameter list and return json with
+   * StandardParameter set
+   */
+  public static String generateDataParametersJSON(List<StandardParameter> listParam,
+      BaseOBObject bob) throws Exception {
+    String json = "";
+    JSONObject jsonMap = new JSONObject();
+    try {
+      for (StandardParameter stdParam : listParam) {
+        if (stdParam.isActive()) {
+          jsonMap.put(stdParam.getName(), DalUtil.getValueFromPath(bob, stdParam.getProperty()));
+        }
+      }
+      json = jsonMap.toString();
+    } catch (JSONException e) {
+      String message = String.format(Utility.messageBD(conn, "smfwhe_errorGenerateJson", language),
+          bob.getIdentifier());
+      throw new Exception(message);
+    }
+    return json;
+  }
+
+  /*
+   * Generate a XML data parameter, take a StandardParameter list and return XML with
+   * StandardParameter set
+   */
+  public static String generateDataParametersXML(List<StandardParameter> listParam, BaseOBObject bob)
+      throws Exception {
+    String json = "";
+    return json;
+  }
+
+  /*
+   * Generate a Customparam, take a Customparam list and return string you can added in the url
+   */
+  public static String generateCustomParameter(List<Customparam> lCustomParam) {
+    String parameters = START_PARAMETER;
+    for (Customparam param : lCustomParam) {
+      if (param.isActive()) {
+        parameters += param.getName() + EQUALS + param.getValue() + AMPERSAND;
+      }
+    }
+    return parameters.substring(0, parameters.length() - 1);
+  }
+}
